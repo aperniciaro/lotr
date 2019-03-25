a098b01b5a0d8b6b8773f5e83a4ea19ef9781cd7
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-03-25 14:16:00 -0400

    added race endpoint actions

--- Controllers/RacesController.cs
+++ Controllers/RacesController.cs
@@ -2,44 +2,67 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using lotr.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace lotr.Controllers
 {
   [Route("api/[controller]")]
   [ApiController]
+
   public class RacesController : ControllerBase
   {
+    private DatabaseContext db;
+    public RacesController()
+    {
+      this.db = new DatabaseContext();
+    }
+
     // GET api/values
     [HttpGet]
-    public ActionResult<IEnumerable<string>> Get()
+    public ActionResult<IList<Race>> GetAllRaces()
     {
-      return new string[] { "value1", "value2" };
+      var results = db.Races.ToList();
+      return results;
     }
 
     // GET api/values/5
     [HttpGet("{id}")]
-    public ActionResult<string> Get(int id)
+    public ActionResult<Race> GetSingleRace(int id)
     {
-      return "value";
+      var race = db.Races.FirstOrDefault(r => r.Id == id);
+      return race;
     }
 
     // POST api/values
     [HttpPost]
-    public void Post([FromBody] string value)
+    public ActionResult<Race> AddRace([FromBody] Race raceToAdd)
     {
+      db.Races.Add(raceToAdd);
+      db.SaveChanges();
+      return raceToAdd;
     }
 
     // PUT api/values/5
     [HttpPut("{id}")]
-    public void Put(int id, [FromBody] string value)
+    public ActionResult<Race> UpdateRace(int id, [FromBody] Race newRaceData)
     {
+      var race = db.Races.FirstOrDefault(r => r.Id == id);
+      race.RaceName = newRaceData.RaceName;
+      race.NativeLanguage = newRaceData.NativeLanguage;
+      race.IsImmortal = newRaceData.IsImmortal;
+      db.SaveChanges();
+      return race;
     }
 
     // DELETE api/values/5
     [HttpDelete("{id}")]
-    public void Delete(int id)
+    public ActionResult DeleteRace(int id)
     {
+      var race = db.Races.FirstOrDefault(r => r.Id == id);
+      db.Races.Remove(race);
+      db.SaveChanges();
+      return Ok();
     }
   }
 }
