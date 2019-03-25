b00b9a0ae8eaf7417074d5f4969ec2e24d196901
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-03-25 13:25:17 -0400

    migrated characters and races

---
+++ Migrations/20190325172400_AddedCharactersAndRaces.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace lotr.Migrations
+{
+    public partial class AddedCharactersAndRaces : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Characters",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    Name = table.Column<string>(nullable: true),
+                    WeaponOfChoice = table.Column<string>(nullable: true),
+                    Profession = table.Column<string>(nullable: true),
+                    Residence = table.Column<string>(nullable: true),
+                    HasWieldedRing = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Characters", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Races",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.SerialColumn),
+                    RaceName = table.Column<string>(nullable: true),
+                    PrimaryLanguage = table.Column<string>(nullable: true),
+                    IsImmortal = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Races", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Characters");
+
+            migrationBuilder.DropTable(
+                name: "Races");
+        }
+    }
+}
