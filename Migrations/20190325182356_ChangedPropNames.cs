d39a14df0b548e52e17d84debc413d0a4902a64b
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-03-25 14:26:45 -0400

    updated migration

---
+++ Migrations/20190325182356_ChangedPropNames.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace lotr.Migrations
+{
+    public partial class ChangedPropNames : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PrimaryLanguage",
+                table: "Races",
+                newName: "NativeLanguage");
+
+            migrationBuilder.RenameColumn(
+                name: "HasWieldedRing",
+                table: "Characters",
+                newName: "HasWieldedOneRing");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "NativeLanguage",
+                table: "Races",
+                newName: "PrimaryLanguage");
+
+            migrationBuilder.RenameColumn(
+                name: "HasWieldedOneRing",
+                table: "Characters",
+                newName: "HasWieldedRing");
+        }
+    }
+}
