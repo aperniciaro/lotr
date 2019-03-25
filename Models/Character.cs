ca5c19ff204dcbb819f69a91fce35f878d7a361c
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-03-25 13:22:41 -0400

    created race model and controller

--- Models/Character.cs
+++ Models/Character.cs
@@ -1,7 +1,17 @@
 namespace lotr.Models
 {
-    public class Character
-    {
-        
-    }
+  public class Character
+  {
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string WeaponOfChoice { get; set; }
+
+    public string Profession { get; set; }
+
+    public string Residence { get; set; }
+
+    public bool HasWieldedRing { get; set; }
+  }
 }
---
+++ Models/Race.cs
@@ -0,0 +1,13 @@
+namespace lotr.Models
+{
+  public class Race
+  {
+    public int Id { get; set; }
+
+    public string RaceName { get; set; }
+
+    public string PrimaryLanguage { get; set; }
+
+    public bool IsImmortal { get; set; }
+  }
+}
