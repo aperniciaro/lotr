fe68e22be40ad7c5e8ec0e15a9e50e4412cc9989
Andrew Perniciaro <andrew.perniciaro@gmail.com>
2019-03-25 14:32:24 -0400

    added actions for character endpoints

--- Controllers/CharactersController.cs
+++ Controllers/CharactersController.cs
@@ -2,44 +2,69 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using lotr.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace lotr.Controllers
 {
   [Route("api/[controller]")]
   [ApiController]
+
   public class CharactersController : ControllerBase
   {
+    private DatabaseContext db;
+    public CharactersController()
+    {
+      this.db = new DatabaseContext();
+    }
+
     // GET api/values
     [HttpGet]
-    public ActionResult<IEnumerable<string>> Get()
+    public ActionResult<IList<Character>> GetAllCharacters()
     {
-      return new string[] { "value1", "value2" };
+      var results = db.Characters.ToList();
+      return results;
     }
 
     // GET api/values/5
     [HttpGet("{id}")]
-    public ActionResult<string> Get(int id)
+    public ActionResult<Character> GetSingleCharacter(int id)
     {
-      return "value";
+      var character = db.Characters.FirstOrDefault(c => c.Id == id);
+      return character;
     }
 
     // POST api/values
     [HttpPost]
-    public void Post([FromBody] string value)
+    public ActionResult<Character> AddCharacter([FromBody] Character characterToAdd)
     {
+      db.Characters.Add(characterToAdd);
+      db.SaveChanges();
+      return characterToAdd;
     }
 
     // PUT api/values/5
     [HttpPut("{id}")]
-    public void Put(int id, [FromBody] string value)
+    public ActionResult<Character> UpdateCharacter(int id, [FromBody] Character newCharacterData)
     {
+      var character = db.Characters.FirstOrDefault(c => c.Id == id);
+      character.Name = newCharacterData.Name;
+      character.WeaponOfChoice = newCharacterData.WeaponOfChoice;
+      character.Profession = newCharacterData.Profession;
+      character.Residence = newCharacterData.Residence;
+      character.HasWieldedOneRing = newCharacterData.HasWieldedOneRing;
+      db.SaveChanges();
+      return character;
     }
 
     // DELETE api/values/5
     [HttpDelete("{id}")]
-    public void Delete(int id)
+    public ActionResult DeleteCharacter(int id)
     {
+      var character = db.Characters.FirstOrDefault(c => c.Id == id);
+      db.Characters.Remove(character);
+      db.SaveChanges();
+      return Ok();
     }
   }
 }
